e7ff685bf484338549a68a4a83701e2450cdd204
Chun Wu <n4rjk00@gmail.com>
2021-03-28 12:26:50 -0600

    re-structure project layout

--- ForFun/lib_playground.py
+++ ForFun/lib_playground.py
@@ -2,5 +2,6 @@
 def myAdd(a, b):
     return a+b
 
+
 if __name__ == "__main__":
     print("---lib_playground.py---")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from lib_playground import myAdd
+import numpy as np
+import tensorflow as tf
+from ForFun.lib_playground import myAdd
 
-print(myAdd(10, 2))
+
+
+def main():
+    print("tensorflow:", tf.__version__)
+    print("numpy:", np.__version__)
+    print(myAdd(10, 2))
+
 
 if __name__ == "__main__":
     print("---main.py---")
+    main()
 
